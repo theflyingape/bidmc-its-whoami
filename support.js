28c62acd8798e4005bc21f71ccf07072b7fea8ee
Robert Hurst <theflyingape@gmail.com>
2018-08-23 08:43:47 -0400

    DHCP naming convention

--- support.js
+++ support.js
@@ -1,5 +1,7 @@
 // BIDMC ITS: Who Am I for Field and Support teams
 //
+// 23-Aug-2018 rhurst
+// Chrome deployment: v1.1.6
 // 28-Jun-2018 rhurst
 // AWS hosting: v1.1.4
 // 10-May-2018 rhurst
@@ -9,11 +11,14 @@
 // 19-Apr-2018 rhurst
 // draft
 
-//const CROSBY = 'https://rhurst-laptop.bidmc.harvard.edu:3333/crosby/'
-//const CROSBY = 'https://cassiopeia.bidmc.harvard.edu/crosby/'
-const CROSBY = 'https://crosby.bidmc.org/crosby/'
+const DEBUG = false;
+const CROSBY = DEBUG ? 'https://rhurst-laptop.bidmc.harvard.edu:3333/crosby/' : 'https://crosby.bidmc.org/crosby/';
 let headers = new Headers();
 //headers.append('Content-Type', 'application/json');
+
+let gcbyou = { '/': { nic:1, wifi:2  } };
+let nic = '';
+let wifi = '';
 
 let info = document.getElementById("info");
 let status = document.getElementById("status");
@@ -25,16 +30,21 @@ document.getElementById("authorize").addEventListener("submit", authorize);
 
 document.getElementById("keyButton").addEventListener("click", findAssetBydeviceId);
 document.getElementById("serialNumberButton").addEventListener("click", findAssetByserialNumber);
-document.getElementById("macAddressButton").addEventListener("click", findAssetBymacAddress);
+document.getElementById("nicAddressButton").addEventListener("click", findAssetBynicAddress);
+document.getElementById("wifiAddressButton").addEventListener("click", findAssetBywifiAddress);
 document.getElementById("assetIdButton").addEventListener("click", findAssetByannotatedAssetId);
 
-document.getElementsByName("annotatedAssetId")[0].addEventListener("change", () => { document.getElementById("patchButton").disabled = false; });
+//document.getElementsByName("annotatedAssetId")[0].addEventListener("change", () => { document.getElementById("patchButton").disabled = false; });
 document.getElementsByName("annotatedLocation")[0].addEventListener("change", () => { document.getElementById("patchButton").disabled = false; });
 document.getElementsByName("annotatedUser")[0].addEventListener("change", () => { document.getElementById("patchButton").disabled = false; });
 document.getElementsByName("notes")[0].addEventListener("change", () => { document.getElementById("patchButton").disabled = false; });
 document.getElementById("patch").addEventListener("submit", patch);
 
-document.getElementById("toOU").addEventListener("change", () => { document.getElementById("moveButton").disabled = false; });
+document.getElementById("toOU").addEventListener("change", () => {
+	namingConvention();
+	document.getElementById("moveButton").disabled = false;
+	document.getElementsByName("reboot")[0].hidden = false;
+});
 document.getElementById("moveTo").addEventListener("submit", moveTo);
 
 function toggleUI()
@@ -42,7 +52,7 @@ function toggleUI()
 	info.hidden = !info.hidden;
 	work.hidden = info.hidden;
 	tech.hidden = !info.hidden;
-	fail();
+	fail(false);
 }
 
 function wait() {
@@ -51,15 +61,21 @@ function wait() {
 	status.src = 'assets/yellow_light.png';
 }
 
-function fail() {
-	console.log('fail');
+function fail(alert=true) {
+	if (alert) {
+		console.log('fail');
+		status.src = 'assets/red_light.png';
+		status.title = `NOT connected to ${CROSBY}`;
+	}
+	else {
+		status.src = 'assets/yellow_light.png';
+		status.title = 'idle';
+	}
 	document.body.style.cursor = '';
 	loadAsset({});
 	work.hidden = true;
 	let logon = document.getElementById("logon");
 	logon.disabled = false;
-	status.src = 'assets/red_light.png';
-	status.title = `NOT connected to ${CROSBY}`;
 	headers = new Headers();
 	document.getElementsByName('password')[0].value = '';
 	let id = document.getElementsByName('id')[0];
@@ -132,7 +148,8 @@ function loadOU() {
 	//let watch: HTMLOptionsCollection = <any>el
 	let el = document.getElementById('toOU');
 	wait();
-	fetch(`${CROSBY}ou/`, { method: 'GET', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
+	fetch(`${CROSBY}ou/`,
+	{ method: 'GET', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
 		light(res.ok);
 		return res.json().then(function (data) {
 			for (let i in data) {
@@ -143,7 +160,21 @@ function loadOU() {
 			}
 			el.selectedIndex = -1;
 		})
-	}).catch(function (err) { fail(); })
+	}).catch(function (err) {
+		console.log(err);
+		fail(); 
+	})
+
+	fetch(`${CROSBY}gc-by-ou.json`,
+	{ method: 'GET', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
+		return res.json().then(function (data) {
+			console.log(data);
+			Object.assign(gcbyou, data);
+		})
+	}).catch(function (err) {
+		console.log(err);
+		fail(); 
+	})
 }
 
 function loadAsset(data) {
@@ -151,9 +182,9 @@ function loadAsset(data) {
 	document.getElementsByName('orgUnitPath')[0].value = data.orgUnitPath || '';
 	document.getElementsByName('model')[0].value = data.model || '';
 	document.getElementsByName('serialNumber')[0].value = data.serialNumber || '';
-	document.getElementsByName('macAddress')[0].value = data.macAddress || '';
+	document.getElementsByName('nicAddress')[0].value = data.ethernetAddress || '';
+	document.getElementsByName('wifiAddress')[0].value = data.macAddress || '';
 	document.getElementsByName('osVersion')[0].value = data.osVersion || '';
-	document.getElementsByName('annotatedAssetId')[0].value = data.annotatedAssetId || '';
 	document.getElementsByName('annotatedLocation')[0].value = data.annotatedLocation || '';
 	document.getElementsByName('annotatedUser')[0].value = data.annotatedUser || '';
 	document.getElementsByName('notes')[0].value = data.notes || '';
@@ -163,8 +194,13 @@ function loadAsset(data) {
 		if (el[i].text == data.orgUnitPath)
 			el.selectedIndex = i;
 
+	document.getElementsByName('annotatedAssetId')[0].value = data.annotatedAssetId || '';
+	nic = document.getElementsByName("nicAddress")[0].value.replace(/:/g,'').toUpperCase();
+	wifi = document.getElementsByName("wifiAddress")[0].value.replace(/:/g,'').toUpperCase();
+
 	document.getElementById("patchButton").disabled = true;
 	document.getElementById("moveButton").disabled = true;
+	document.getElementsByName("reboot")[0].hidden = true;
 }
 
 function findAssetBydeviceId() {
@@ -195,8 +231,21 @@ function findAssetByserialNumber() {
 	}
 }
 
-function findAssetBymacAddress() {
-	let macAddress = document.getElementsByName("macAddress")[0].value.replace(/:/g,'').toLowerCase();
+function findAssetBynicAddress() {
+	let macAddress = document.getElementsByName("nicAddress")[0].value.replace(/:/g,'').toLowerCase();
+	if (macAddress) {
+		wait();
+		fetch(`${CROSBY}devices/?ethernet_mac=${macAddress}`, { method: 'GET', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
+			light(res.ok);
+			return res.json().then(function (data) {
+				loadAsset(data);
+			})
+		}).catch(function (err) { fail(); })
+	}
+}
+
+function findAssetBywifiAddress() {
+	let macAddress = document.getElementsByName("wifiAddress")[0].value.replace(/:/g,'').toLowerCase();
 	if (macAddress) {
 		wait();
 		fetch(`${CROSBY}devices/?wifi_mac=${macAddress}`, { method: 'GET', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
@@ -224,24 +273,30 @@ function findAssetByannotatedAssetId() {
 function moveTo() {
 	let deviceId = document.getElementsByName("key")[0].value;
 	let el = document.getElementById('toOU');
-	let ou = el[el.selectedIndex].text;	//	.value "fails" if it has a leading "0", go figure
+	let ou = '';
+	if (el.selectedIndex >= 0)
+		ou = el[el.selectedIndex].text;	//	.value "fails" if it has a leading "0", go figure
 	if (deviceId && ou) {
 		wait();
 		fetch(`${CROSBY}move/?id=${deviceId}&ou=${ou}`, { method: 'POST', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
-			light(res.ok);
-			findAssetBydeviceId();
+			//light(res.ok);
+			//findAssetBydeviceId();
+			patch();
 		}).catch(function (err) { fail(); })
 	}
 }
 
 function patch() {
 	let deviceId = document.getElementsByName("key")[0].value;
-	let annotatedAssetId = document.getElementsByName('annotatedAssetId')[0].value;
-	let annotatedLocation = document.getElementsByName('annotatedLocation')[0].value;
-	let annotatedUser = document.getElementsByName('annotatedUser')[0].value;
-	let notes = document.getElementsByName('notes')[0].value;
 	if (deviceId) {
 		wait();
+
+		namingConvention();
+		let annotatedAssetId = document.getElementsByName('annotatedAssetId')[0].value;
+		let annotatedLocation = document.getElementsByName('annotatedLocation')[0].value;
+		let annotatedUser = document.getElementsByName('annotatedUser')[0].value;
+		let notes = document.getElementsByName('notes')[0].value;
+
 		fetch(`${CROSBY}patch/?id=${deviceId}&annotatedAssetId=${annotatedAssetId}&annotatedLocation=${annotatedLocation}&annotatedUser=${annotatedUser}&notes=${notes}`,
 		{ method: 'POST', headers: headers, credentials: 'same-origin', mode: 'cors' }).then(function (res) {
 			light(res.ok);
@@ -249,5 +304,36 @@ function patch() {
 				findAssetBydeviceId();
 			})
 		}).catch(function (err) { fail(); })
+	}
+}
+
+// assert our naming convention
+function namingConvention() {
+	//	Google Chrome + MAC address
+	let result = 'GC-' + (nic || wifi);
+
+	let el = document.getElementById('toOU');
+	let ou = '';
+	if (el.selectedIndex >= 0)
+		ou = el[el.selectedIndex].text;	//	.value "fails" if it has a leading "0", go figure
+	if (ou) {
+		let keys = Object.keys(gcbyou).sort();
+		let key = ''
+		keys.forEach(k => {
+			if (ou.startsWith(k)) key = k
+		});
+		if (key) {
+			if (key.nic < key.wifi)
+				result = gcbyou[key].prefix + (nic || wifi);
+			else
+				result = gcbyou[key].prefix + (wifi || nic);
+		}
+	}
+
+	let annotatedAssetId = document.getElementsByName('annotatedAssetId')[0].value.toUpperCase();
+	//.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "")
+	if (result != annotatedAssetId) {
+		document.getElementsByName('annotatedAssetId')[0].value = result;
+		document.getElementsByName("reboot")[0].hidden = false;
 	}
 }
